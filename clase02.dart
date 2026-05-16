36737b2fedec1455042195633f0ba4575f762e10
victoria <estu75939160@institutobolivar.edu.pe>
2026-05-15 21:50:42 -0500

    tipos de variables en DartPad 02

---
+++ clase02.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('Suma de nuemros');
+  int n1 = 20;
+  int n2 = 34;
+  int suma = n1 + n2;
+  print(n1);
+  print(n2);
+  print(suma);
+
+  print('Area de un cuadrado');
+  int lado = 25;
+  int area = lado * lado;
+  print(lado);
+  print(area);
+}
