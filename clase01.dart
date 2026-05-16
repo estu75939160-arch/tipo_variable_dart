43f2d7e57f4c0b6ecb730ea97e04a9db93c9d327
victoria <estu75939160@institutobolivar.edu.pe>
2026-05-15 20:14:50 -0500

    tipos de variables en DartPad

---
+++ clase01.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Hello people');
+  int edad = 22;
+  double pi = 3.14;
+  bool esActivo = true;
+  String saludo = "hola";
+
+  String mensaje = "Mi edad es $edad";
+  print(mensaje);
+}
